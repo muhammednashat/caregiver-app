9473aeb4274d2127afce032bef44f8261ed05079
Mohammed Nashat <mnashat.dev@gmail.com>
2024-07-20 14:52:10 +0200

    create AzcarFragment.kt

---
+++ app/src/main/java/mnshat/dev/myproject/users/patient/main/tools/AzcarFragment.kt
@@ -0,0 +1,23 @@
+package mnshat.dev.myproject.users.patient.main.tools
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mnshat.dev.myproject.R
+import mnshat.dev.myproject.base.BaseFragment
+import mnshat.dev.myproject.databinding.FragmentAddAzcarBinding
+import mnshat.dev.myproject.users.patient.main.BasePatientFragment
+
+
+
+class AzcarFragment : BasePatientFragment<FragmentAddAzcarBinding>() {
+    override fun initializeViews() {
+
+    }
+
+    override fun getLayout()= R.layout.fragment_azcar
+
+
+}
