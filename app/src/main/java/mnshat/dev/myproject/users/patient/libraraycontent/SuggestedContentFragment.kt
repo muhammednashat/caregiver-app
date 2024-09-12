8f29077b8947d3811e4442743625aafc32495311
muhammednashat <mnashat.dev@gmail.com>
2024-09-12 15:15:00 +0200

    create  Content.kt

---
+++ app/src/main/java/mnshat/dev/myproject/users/patient/libraraycontent/SuggestedContentFragment.kt
@@ -0,0 +1,71 @@
+package mnshat.dev.myproject.users.patient.libraraycontent
+
+
+import androidx.lifecycle.ViewModelProvider
+import mnshat.dev.myproject.R
+import mnshat.dev.myproject.adapters.CustomizedContentLibraryAdapter
+import mnshat.dev.myproject.base.BaseBottomSheetDialogFragment
+import mnshat.dev.myproject.databinding.FragmentSuggestedContentBinding
+import mnshat.dev.myproject.factories.LibraryViewModelFactory
+import mnshat.dev.myproject.interfaces.OnItemLibraryContentClicked
+import mnshat.dev.myproject.model.LibraryContent
+import mnshat.dev.myproject.util.AUDIO
+import mnshat.dev.myproject.util.ENGLISH_KEY
+
+class SuggestedContentFragment : BaseBottomSheetDialogFragment<FragmentSuggestedContentBinding>(),
+
+    OnItemLibraryContentClicked {
+
+    private lateinit var viewModel: LibraryViewModel
+    private lateinit var mOnItemLibraryContentClicked: OnItemLibraryContentClicked
+
+    override fun getLayout() = R.layout.fragment_suggested_content
+
+    override fun initializeViews() {
+        if (currentLang != ENGLISH_KEY) {
+            binding.close.setBackgroundDrawable(resources.getDrawable(R.drawable.background_back_right))
+            binding.root.setBackgroundDrawable(resources.getDrawable(R.drawable.corner_top_lift))
+        }
+        initViewModel()
+
+        setRecyclerMostCommon(viewModel.getContentsBasedType(AUDIO))
+    }
+
+    override fun setupClickListener() {
+        super.setupClickListener()
+
+        binding.close.setOnClickListener {
+            dismiss()
+        }
+
+    }
+
+
+    private fun initViewModel() {
+        val factory = LibraryViewModelFactory(sharedPreferences, activity?.application!!)
+        viewModel = ViewModelProvider(requireActivity(), factory)[LibraryViewModel::class.java]
+    }
+
+    private fun setRecyclerMostCommon(libraryContent: List<LibraryContent>?) {
+
+        binding.recyclerView.apply {
+
+            adapter = CustomizedContentLibraryAdapter(
+                libraryContent,
+                requireActivity(),
+                sharedPreferences,
+                this@SuggestedContentFragment
+            )
+        }
+    }
+
+    override fun onItemClicked(type: String, index: Int, content: String) {
+        dismiss()
+        mOnItemLibraryContentClicked.onItemClicked(type, index, content)
+    }
+
+    fun setOnItemLibraryContent(onItemLibraryContentClicked: OnItemLibraryContentClicked) {
+        mOnItemLibraryContentClicked = onItemLibraryContentClicked
+    }
+
+}
