91320d0a9af2ccd893bfa349c579b9582785da37
muhammednashat <mnashat.dev@gmail.com>
2024-09-22 14:01:20 +0200

    create  fragment_caregiver_home.xml

--- app/src/main/java/mnshat/dev/myproject/users/caregiver/CaregiverHomeFragment.kt
+++ app/src/main/java/mnshat/dev/myproject/users/caregiver/CaregiverHomeFragment.kt
@@ -5,24 +5,20 @@ import androidx.navigation.fragment.findNavController
 import mnshat.dev.myproject.R
 import mnshat.dev.myproject.databinding.FragmentCaregiverHomeBinding
 import mnshat.dev.myproject.users.patient.libraraycontent.LibraryActivity
+import mnshat.dev.myproject.util.log
 
 class CaregiverHomeFragment : BaseCaregiverFragment<FragmentCaregiverHomeBinding>() {
 
 
 
-    override fun initializeViews() {
-
-     }
-
     override fun getLayout()= R.layout.fragment_caregiver_home
 
     override fun setupClickListener() {
         super.setupClickListener()
-
-        binding.btnContinue.setOnClickListener {
+        binding.enter.setOnClickListener {
             findNavController().navigate(R.id.action_caregiverHomeFragment_to_usersFragment)
         }
-        //
+
         binding.rootEducationalContent.setOnClickListener {
             startActivity(Intent(requireActivity(), LibraryActivity::class.java))
         }
