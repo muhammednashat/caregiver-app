78d8d7ad751710f9a70d06333a7ee32bf0cf7cd6
muhammednashat <mnashat.dev@gmail.com>
2025-01-27 17:13:55 +0200

    update basefragment

--- app/src/main/java/mnshat/dev/myproject/users/patient/supporters/SupporterDetailsFragment.kt
+++ app/src/main/java/mnshat/dev/myproject/users/patient/supporters/SupporterDetailsFragment.kt
@@ -9,22 +9,55 @@ import androidx.navigation.fragment.navArgs
 import mnshat.dev.myproject.R
 import mnshat.dev.myproject.databinding.FragmentSupporterDetailsBinding
 import mnshat.dev.myproject.firebase.FirebaseService
+import mnshat.dev.myproject.model.Permissions
 import mnshat.dev.myproject.model.RegistrationData
 import mnshat.dev.myproject.util.ENGLISH_KEY
 import mnshat.dev.myproject.util.STATUS
+import mnshat.dev.myproject.util.log
+
+
+
+//  test with tow devices
+
+
+
+
+
 
 
 class SupporterDetailsFragment : BaseSupporterFragment<FragmentSupporterDetailsBinding>() {
     private lateinit var supporter: RegistrationData
     private lateinit var action: NavDirections
-
+   private var currentPermissions = mutableListOf(false,false,false)
+   private  val modifiedPermissions = mutableListOf(false,false,false)
     override fun getLayout() = R.layout.fragment_supporter_details
+
+
     override fun initializeViews() {
         val args: SupporterDetailsFragmentArgs by navArgs()
         supporter = args.supporter
         setUpUiSupporter(args.supporter)
+        setUpSwitchesListener()
         if (currentLang != ENGLISH_KEY) {
             binding.icBack.setBackgroundDrawable(resources.getDrawable(R.drawable.background_back_right))
+        }
+    }
+
+    private fun setUpSwitchesListener() {
+    log(currentPermissions.toString())
+    log(modifiedPermissions.toString())
+     binding.permissions.viewDailyProgramDetails.setOnCheckedChangeListener { _, isChecked ->updateSwitching(0,isChecked)}
+     binding.permissions.viewMoodTrackingDetails.setOnCheckedChangeListener { _, isChecked ->updateSwitching(1,isChecked)}
+     binding.permissions.allowPrivateMessages.setOnCheckedChangeListener { _, isChecked ->updateSwitching(2,isChecked)}
+    }
+    private fun updateSwitching(index:Int, isChecked:Boolean){
+        modifiedPermissions[index] = isChecked
+        log(currentPermissions.toString())
+        log(modifiedPermissions.toString())
+        if (currentPermissions == modifiedPermissions){
+            binding.save.visibility = View.GONE
+        }else{
+            binding.save.visibility = View.VISIBLE
         }
     }
 
@@ -35,6 +68,10 @@ class SupporterDetailsFragment : BaseSupporterFragment<FragmentSupporterDetailsB
         }
         binding.icMore.setOnClickListener {
             showPopupMenu(it)
+        }
+        binding.save.setOnClickListener {
+            showProgressDialog()
+            updateSupporterPermissionsRemotely(modifiedPermissions)
         }
 
         binding.permissions.viewReporting.setOnClickListener {
@@ -54,16 +91,39 @@ class SupporterDetailsFragment : BaseSupporterFragment<FragmentSupporterDetailsB
 
     }
 
+    private fun updateSupporterPermissionsRemotely(modifiedPermissions: MutableList<Boolean>) {
+        val permissions = Permissions(allowDailyProgramDetails = modifiedPermissions[0] , allowMoodTrackingDetails =modifiedPermissions[1] , allowPrivateMessages= modifiedPermissions[2])
+        val map = mapOf("permissions" to permissions)
+        FirebaseService.updateItemsProfileUser(supporter.id!!, map) {
+            if (it) {
+             currentPermissions = modifiedPermissions.toMutableList()
+             binding.save.visibility = View.GONE
+            } else {
+
+            }
+            dismissProgressDialog()
+        }
+    }
+
     private fun setUpUiSupporter(supporter: RegistrationData) {
         binding.nameSupporter.text = supporter.name
         checkStatus()
         supporter.permissions?.let {
-            if (it.allowDailyProgramDetails) binding.permissions.viewDailyProgramDetails.isChecked =
-                true
-            if (it.allowMoodTrackingDetails) binding.permissions.viewMoodTrackingDetails.isChecked =
-                true
-
-            if (it.allowPrivateMessages) binding.permissions.allowPrivateMessages.isChecked = true
+            if (it.allowDailyProgramDetails){
+                binding.permissions.viewDailyProgramDetails.isChecked =true
+                currentPermissions[0] = true
+                modifiedPermissions[0] = true
+            }
+            if (it.allowMoodTrackingDetails) {
+                binding.permissions.viewMoodTrackingDetails.isChecked =true
+                currentPermissions[1] = true
+                modifiedPermissions[1] = true
+            }
+            if (it.allowPrivateMessages) {
+                binding.permissions.allowPrivateMessages.isChecked =true
+                currentPermissions[2] = true
+                modifiedPermissions[2] = true
+            }
         }
     }
 
