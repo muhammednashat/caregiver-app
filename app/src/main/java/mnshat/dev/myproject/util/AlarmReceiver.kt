02a78588e43bb529b8d89506d0c1222e4dbbd4a1
muhammednashat <mnashat.dev@gmail.com>
2025-02-20 15:19:34 +0200

    add broadCast receiver to listen for boot completed

--- app/src/main/java/mnshat/dev/myproject/util/AlarmReceiver.kt
+++ app/src/main/java/mnshat/dev/myproject/util/AlarmReceiver.kt
@@ -21,9 +21,30 @@ import java.util.Date
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
+        sendNotification(" لا تنسى القيام بمهام اليوم tretwergf" , context)
+
         val workManager = WorkManager.getInstance(context)
         val request = OneTimeWorkRequestBuilder<MyWorkManager>().build()
         workManager.enqueue(request)
+    }
+    @SuppressLint("MissingPermission")
+
+    fun sendNotification(text:String,context: Context){
+        val intent = Intent(context, UserScreensActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(context, CALENDER_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("My Application")
+            .setContentText(text)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(15 ,builder.build())
+
     }
 
 }
