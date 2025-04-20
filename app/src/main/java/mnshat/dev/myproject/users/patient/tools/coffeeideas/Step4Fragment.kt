70e8e7e91f5e7a521fd1291014a018df583a409f
muhammednashat <mnashat.dev@gmail.com>
2025-04-20 17:19:02 +0200

    today i design screen for step 1

---
+++ app/src/main/java/mnshat/dev/myproject/users/patient/tools/coffeeideas/Step4Fragment.kt
@@ -0,0 +1,21 @@
+package mnshat.dev.myproject.users.patient.tools.coffeeideas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mnshat.dev.myproject.R
+
+
+class Step4Fragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_step4, container, false)
+    }
+
+}
