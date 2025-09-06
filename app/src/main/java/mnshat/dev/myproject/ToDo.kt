1d79136d2a95009974c35fc923e7d3f45a06d4a4
muhammednashat <mnashat.dev@gmail.com>
2025-09-06 15:57:59 +0300

    Refactor: Remove completed tasks from ToDo list

--- app/src/main/java/mnshat/dev/myproject/ToDo.kt
+++ app/src/main/java/mnshat/dev/myproject/ToDo.kt
@@ -9,17 +9,10 @@ package mnshat.dev.myproject
  *
  *
  * rest day tasks after updating relagion
- * crash when  try to open a post
+ *
  *
  * test caregiver posts
  * store caregiver and test partner email , partner id
- *
- *
- *
- *  
- *
- *
- *
  *
  *
  *
@@ -31,20 +24,8 @@ package mnshat.dev.myproject
 //ToDo
 /**
  *
- * 
  * * lang at first time
- *  *
  *  * check splash screen
- *  *
- *
- *  *
- *
- *  *
- *  * show onboarding screens with each new account
- *  *  check  connect with supporter and it's permission
- *
- *  solve bug when login after logout related to room
- *
  *
  *
  */
